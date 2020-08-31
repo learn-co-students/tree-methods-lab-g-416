ffa9b6dbf421c6615ed7cb5175f71a872e5c13de
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-31 13:37:29 +0100

    Done.

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ function inOrder(node) {
 }
 
 function findOrAdd(root, newNode) {
-  if (root.data > newNode.data) {
-    root.right ? findOrAdd(root.right, newNode) : (root.right = newNode);
-  } else {
-    root.left ? findOrAdd(root.left, newNode) : (root.left = newNode);
+  if (root.data === newNode.data) {
+    return true;
   }
+  if (root.data < newNode.data) {
+    return root.right ? findOrAdd(root.right, newNode) : (root.right = newNode);
+  } else {
+    return root.left ? findOrAdd(root.left, newNode) : (root.left = newNode);
+  }
+}
+
+function max(node) {
+  return node.right ? max(node.right) : node;
+}
+
+function min(node) {
+  return node.left ? min(node.left) : node;
 }
