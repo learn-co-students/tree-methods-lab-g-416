70b65dfe8f7e2c196d8dfa83e2cce5e707a7f7f2
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-31 13:03:06 +0100

    in Order function

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function inOrder(node) {
   if (node.left) {
-    inOrder(node.left)
-  } 
-  console.log(node.data)
-  if(node.right){
-    inOrder(node.right)
+    inOrder(node.left);
+  }
+  console.log(node.data);
+  if (node.right) {
+    inOrder(node.right);
+  }
+}
+
+function findOrAdd(root, newNode) {
+  if (root.data > newNode.data) {
+    root.right ? findOrAdd(root.right, newNode) : (root.right = newNode);
+  } else {
+    root.left ? findOrAdd(root.left, newNode) : (root.left = newNode);
   }
 }
