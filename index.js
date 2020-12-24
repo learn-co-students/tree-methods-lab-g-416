daddb05d7f9d407b7723b314606a59dde1d638f2
osha7 <ogroetz@gmail.com>
2020-12-24 11:49:28 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function inOrder(currentNode){
+    if(currentNode.left){
+      inOrder(currentNode.left)
+    }
+    console.log(currentNode.data)
+    if(currentNode.right){
+      inOrder(currentNode.right)
+    }
+}
+
+function findOrAdd(currentNode, newNode) {
+    if (newNode.data < currentNode.data) {
+        let leftNode = currentNode.left
+        if(leftNode) {
+            return findOrAdd(leftNode, newNode)
+        } else {
+            currentNode.left = newNode
+        }
+    } else if (newNode.data > currentNode.data) {
+        let rightNode = currentNode.right
+        if(rightNode) {
+            return findOrAdd(rightNode, newNode)
+        } else {
+            currentNode.right = newNode
+        }
+    } else {
+        return true
+    }
+}
+
+function max(currentNode) {
+    if (currentNode.right) {
+        return max(currentNode.right)
+    } else {
+        return currentNode
+    }
+}
+
+function min(currentNode) {
+    if (currentNode.left) {
+        return min(currentNode.left)
+    } else {
+        return currentNode
+    }
+}
