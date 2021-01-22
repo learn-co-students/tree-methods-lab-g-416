e32457521ee5689046bc32587da6edffff8d56ce
NickSpangler <Spangleyman@mac.com>
2021-01-22 15:32:28 -0500

    solve findOrAdd()

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ function inOrder(currentNode) {
         inOrder(currentNode.right)
     }
 }
+
+function findOrAdd(rootNode, newNode) {
+    let currentNode = rootNode;
+    if (newNode.data < currentNode.data) {
+        currentNode = rootNode.left;
+        if (currentNode) {
+            findOrAdd(currentNode, newNode)
+        } else {
+            rootNode.left = newNode;
+        }
+    } else if (newNode.data > currentNode.data) {
+        currentNode = rootNode.right;
+        if (currentNode) {
+            findOrAdd(currentNode, newNode);
+        } else {
+            rootNode.right = newNode;
+        }
+    }
+}
