39a930d1bcc41908fa2d4a9d8bc48ec7de4f9104
NickSpangler <Spangleyman@mac.com>
2021-01-22 15:24:14 -0500

    solve inOrder()

---
+++ index.js
@@ -0,0 +1,9 @@
+function inOrder(currentNode) {
+    if (currentNode.left) {
+        inOrder(currentNode.left)
+    }
+    console.log(currentNode.data)
+    if (currentNode.right) {
+        inOrder(currentNode.right)
+    }
+}
