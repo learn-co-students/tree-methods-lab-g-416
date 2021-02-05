be0fa879342c7e954d025d096aceca9a97d99a22
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-05 16:13:52 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function inOrder(currentNode){
+    if(currentNode.left){
+      inOrder(currentNode.left)
+    }
+    console.log(currentNode.data)
+    if(currentNode.right){
+      inOrder(currentNode.right)
+    }
+    console.log(currentNode.data)
+  }
+
