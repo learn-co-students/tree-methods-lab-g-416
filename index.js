2e54093ee78b664a301291bb67692feeb12ff4ac
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-06 15:47:16 -0500

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,43 @@ function inOrder(currentNode){
     console.log(currentNode.data)
   }
 
+  function findOrAdd(rootNode, newNode){
+    let currentNode = rootNode
+    console.log("Line 14. newNode.data=", newNode.data, " currentNode.data=", currentNode.data)
+      if (newNode.data==rootNode.data) {
+        return true
+      }
+    else if(newNode.data < rootNode.data){
+        currentNode = rootNode.left
+        if(currentNode){
+return findOrAdd(currentNode, newNode)
+        } else {
+         return rootNode.left = newNode
+        }
+    } else if(newNode.data > rootNode.data) {
+      currentNode = rootNode.right
+      if(currentNode){
+      return findOrAdd(currentNode, newNode)
+      } else {
+       return rootNode.right = newNode
+      }
+    }
+  }
+
+  
+  function max(currentNode){
+    if(currentNode.right){
+      return max(currentNode.right)
+    } else {
+      return currentNode;
+    }
+  }
+  
+  function min(currentNode){
+    if(currentNode.left){
+      return min(currentNode.left)
+    } else {
+      return currentNode;
+    }
+  }
+  
